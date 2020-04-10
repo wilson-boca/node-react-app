dc2e3e3d3ba11a0c32474eeb0ed15fafeca197cb
Wilson <wilson.boca@gmail.com>
2020-04-09 21:53:55 -0300

    Adding Providers and Schedules

--- src/database/index.js
+++ src/database/index.js
@@ -1,8 +1,10 @@
 import Sequelize from 'sequelize';
 import databaseConfig from '../config/database';
 import User from '../app/models/User';
+import File from '../app/models/File';
+import Appointment from '../app/models/Appointment';
 
-const models = [User];
+const models = [User, File, Appointment];
 
 class Database {
   constructor() {
@@ -12,6 +14,9 @@ class Database {
   init() {
     this.connection = new Sequelize(databaseConfig);
     models.map((model) => model.init(this.connection));
+    models.map(
+      (model) => model.associate && model.associate(this.connection.models)
+    );
   }
 }
 
--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,26 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 import UserController from './app/controllers/UserController';
 import SessionControler from './app/controllers/SessionController';
 import authMiddleware from './app/middlewares/auth';
+import FileController from './app/controllers/FileControllers';
+import ProviderController from './app/controllers/ProviderController';
+import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/ScheduleController';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionControler.store);
 
 routes.use(authMiddleware);
 routes.put('/users', UserController.update);
+routes.post('/files', upload.single('file'), FileController.store);
+routes.get('/providers', ProviderController.index);
+routes.post('/appointments', AppointmentController.store);
+routes.get('/appointments', AppointmentController.index);
+routes.get('/schedules', ScheduleController.index);
 
 export default routes;
